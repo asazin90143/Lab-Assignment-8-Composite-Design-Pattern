558aebea90364a20c0da76d9bf8a560330c1d07a
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2026-04-28 02:41:12 +0000

    update student

--- Student.java
+++ Student.java
@@ -1,33 +1,4 @@
-// Individual Teacher entity [cite: 8, 9]
-class Teacher implements UniversityComponent {
-    private String name;
-    private String subject;
-    private double salary;
-
-    public Teacher(String name, String subject, double salary) {
-        this.name = name;
-        this.subject = subject;
-        this.salary = salary;
-    }
-
-    @Override
-    public void displayDetails(String indent) {
-        System.out.println(indent + "Teacher: " + name + " (Subject: " + subject + ", Salary: " + salary + ")");
-    }
-
-    @Override
-    public int getStudentCount() {
-        return 0;
-    }
-
-    @Override
-    public double getBudget() {
-        return salary;
-    } // Budget is their salary [cite: 19]
-}
-
-// Individual Student entity [cite: 10, 11]
-class Student implements UniversityComponent {
+public class Student implements UniversityComponent {
     private String name;
     private String studentID;
     private double tuitionFee;
@@ -51,5 +22,5 @@ class Student implements UniversityComponent {
     @Override
     public double getBudget() {
         return -tuitionFee;
-    } // Budget is negative tuition [cite: 19]
+    } // Student budget is negative tuition [cite: 19]
 }

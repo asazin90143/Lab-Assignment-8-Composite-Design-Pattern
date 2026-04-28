ff5d2f5bbc7284b9c5eb21772d5b0719cf090d6a
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2026-04-28 02:41:47 +0000

    update colloge

--- College.java
+++ College.java
@@ -1,39 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Container for Teachers and Students [cite: 6, 7]
-class Department implements UniversityComponent {
-    private String name;
-    private List<UniversityComponent> components = new ArrayList<>();
-
-    public Department(String name) {
-        this.name = name;
-    }
-
-    public void add(UniversityComponent component) {
-        components.add(component);
-    }
-
-    @Override
-    public void displayDetails(String indent) {
-        System.out.println(indent + "Department: " + name);
-        for (UniversityComponent c : components)
-            c.displayDetails(indent + "  ");
-    }
-
-    @Override
-    public int getStudentCount() {
-        return components.stream().mapToInt(UniversityComponent::getStudentCount).sum();
-    }
-
-    @Override
-    public double getBudget() {
-        return components.stream().mapToDouble(UniversityComponent::getBudget).sum();
-    }
-}
-
-// High-level unit that can contain anything [cite: 4, 5]
-class College implements UniversityComponent {
+public class College implements UniversityComponent {
     private String name;
     private List<UniversityComponent> children = new ArrayList<>();
 
@@ -54,11 +22,13 @@ class College implements UniversityComponent {
 
     @Override
     public int getStudentCount() {
+        // Total students including departments and sub-colleges [cite: 14]
         return children.stream().mapToInt(UniversityComponent::getStudentCount).sum();
     }
 
     @Override
     public double getBudget() {
+        // Sum of budgets of its internal units [cite: 17]
         return children.stream().mapToDouble(UniversityComponent::getBudget).sum();
     }
 }
